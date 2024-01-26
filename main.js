8f586eb51d7e52505293c488ff3b1d6ccc3b17c8
kfir <kfir.kotler@gmail.com>
2024-01-26 19:51:35 +0200

    Game logic Craeted at user turn

--- main.js
+++ main.js
@@ -2,94 +2,104 @@ let arrRound = [];
 let gameCounter; // Game counter
 let userCounter;
 let isUserTurn = false
+// let numberVlaueGuess = null;
 
 
-// Function (A)
-// Resets the game and ready to play
-// This does NOT start the game, you need to press a button to start the game
 function initGame() {
-    // console.clear();
     gameCounter = userCounter = 0;
     arrRound = [];
     console.log('Game initiated...');
+    isUserTurn = false
 }
 
 // Function (B)
 async function playRound() {
-    // 1. Create step and add as a last element to the array
+
     const newStep = createStep();
     arrRound.push(newStep);
 
     await pcTurn()
-    console.log("FINISHED?");
-    userTurn();
     renderRoundCounter()
 }
 
-function userTurn() {
+function userTurn(guess) {
+    if (isUserTurn) {
+        if (guessValidation(guess, userCounter)) {
+            userCounter++
+            console.log("userCounter-------", userCounter);
+            console.log(userCounter, arrRound.length);
+            if (userCounter >= arrRound.length) {
+                userCounter = 0
+                playRound()
+            }
+            console.log("GUESSED CORRECTLY");
+        } else {
+            initGame()
+            return console.log("Wrong guess");
+        }
 
-
-    gameCounter++;
-    playRound()
-
+    } else {
+        return console.log("Not your turn");
     }
-   
+
+
+
+}
+
 
 
 function pcTurn() {
     return new Promise((resolve) => {
         isUserTurn = false
-        console.log(isUserTurn);
         let arryIntervalCounter = 0
         const intervalID = setInterval(() => {
             if (arryIntervalCounter !== arrRound.length) {
-                console.log(arrRound[arryIntervalCounter]);
+                console.log("From interval of pcTurn", arrRound[arryIntervalCounter]);
                 arryIntervalCounter++
             } else {
                 clearInterval(intervalID)
                 isUserTurn = true
-                console.log(isUserTurn);
                 resolve()
             }
 
         }, 1000);
     })
-   
+
 
 }
 
 
 
 function renderRoundCounter() {
-    
+
     gameRoundDisplay.innerText = gameCounter
 }
 
 
-// Function (C)
-// 1. Creates a random number from 1 to 4
-// 2. Returns the result and/or the color (צבע שמייצג את המספר)
 function createStep() {
     return Math.floor(Math.random() * 4) + 1;
 }
 
-
-
 function handleClickOnSimonBtn(event) {
-    console.log(event.target.id);
+
+
     if (event.target.id === "green") {
-        return 1
+        return userTurn(1)
     }
     if (event.target.id === "red") {
-        return 2
+        return userTurn(2)
     }
     if (event.target.id === "yellow") {
-        return 3
+        return userTurn(3)
     }
     if (event.target.id === "blue") {
-        return 4
+        return userTurn(4)
     }
 
+}
+
+function guessValidation(number, arryIndex) {
+    return number === arrRound[arryIndex]
 }
 
 
@@ -105,4 +115,6 @@ document.getElementById('btnStart').addEventListener('click', playRound);
 document.getElementById('green').addEventListener('click', handleClickOnSimonBtn);
 document.getElementById('red').addEventListener('click', handleClickOnSimonBtn);
 document.getElementById('blue').addEventListener('click', handleClickOnSimonBtn);
-document.getElementById('Yellow').addEventListener('click', handleClickOnSimonBtn);
+document.getElementById('yellow').addEventListener('click', handleClickOnSimonBtn);
+
+
