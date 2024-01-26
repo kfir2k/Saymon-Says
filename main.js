034fac4e2b37c35ff9174462cf41d3b57a8b901a
Kfir Kotler <kfir.kotler@gmail.com>
2024-01-26 17:52:22 +0200

    updated

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let arrRound = [];
 let gameCounter; // Game counter
 let userCounter;
-let arrLengthAsIndex
+let isUserTurn = false
+
 
 // Function (A)
 // Resets the game and ready to play
@@ -14,48 +15,46 @@ function initGame() {
 }
 
 // Function (B)
-function playRound() {
+async function playRound() {
     // 1. Create step and add as a last element to the array
     const newStep = createStep();
     arrRound.push(newStep);
-    arrLengthAsIndex = arrRound.length - 1
-    console.log(newStep);
-    console.log(arrRound);
-    
 
-
-    renderRoundCounter()
+    await pcTurn()
+    console.log("FINISHED?");
     userTurn();
+    renderRoundCounter()
 }
 
 function userTurn() {
 
 
-    for (let i of arrRound) {
-        const inputUser = prompt("Please enter your guess")
-        if (+inputUser === i) {
-            console.log();
-            console.log('You guessed correctly');
-
-        } else {
-            console.log('You lose, game over');
-            initGame();
-            return
-            
-        }
-    }
     gameCounter++;
-    console.log("test", gameCounter);
     playRound()
 
     }
    
 
 
-function pcTurn(){
+function pcTurn() {
+    return new Promise((resolve) => {
+        isUserTurn = false
+        console.log(isUserTurn);
+        let arryIntervalCounter = 0
+        const intervalID = setInterval(() => {
+            if (arryIntervalCounter !== arrRound.length) {
+                console.log(arrRound[arryIntervalCounter]);
+                arryIntervalCounter++
+            } else {
+                clearInterval(intervalID)
+                isUserTurn = true
+                console.log(isUserTurn);
+                resolve()
+            }
 
-
-
+        }, 1000);
+    })
+   
 
 }
 
@@ -74,6 +73,36 @@ function createStep() {
     return Math.floor(Math.random() * 4) + 1;
 }
 
+
+
+function handleClickOnSimonBtn(event) {
+    console.log(event.target.id);
+    if (event.target.id === "green") {
+        return 1
+    }
+    if (event.target.id === "red") {
+        return 2
+    }
+    if (event.target.id === "yellow") {
+        return 3
+    }
+    if (event.target.id === "blue") {
+        return 4
+    }
+
+}
+
+
+
+
+
+
+
+
 initGame();
 const gameRoundDisplay = document.getElementById("gameRoundDisplay")
 document.getElementById('btnStart').addEventListener('click', playRound);
+document.getElementById('green').addEventListener('click', handleClickOnSimonBtn);
+document.getElementById('red').addEventListener('click', handleClickOnSimonBtn);
+document.getElementById('blue').addEventListener('click', handleClickOnSimonBtn);
+document.getElementById('Yellow').addEventListener('click', handleClickOnSimonBtn);
